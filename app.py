3eb5272d4745e72aa09202af0a1196811605d1c6
Joalison Matheus <joalison.matheus@moorse.io>
2023-09-21 10:41:51 -0300

    added dotenv

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask
 from flask_restful import Api
 from helpers.database import db, migrate
 from helpers.confCors import cors
+import os
+from dotenv import load_dotenv
 
 from resources.funcionarios import Funcionarios, FuncionarioId
 from resources.enderecos import Enderecos, EnderecoId
 
+load_dotenv()
+
+postgresUser = os.getenv("POSTGRES_USER")
+postgresPassword = os.getenv("POSTGRES_PASSWORD")
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:senha@localhost:5432/Educa365"
+
+DB_URL = f"postgresql://{postgresUser}:{postgresPassword}@localhost:5432/Educa365"
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -21,7 +30,7 @@ api.add_resource(FuncionarioId, '/funcionarios/<string:id>')
 
 # Enderecos - Resource
 api.add_resource(Enderecos, '/enderecos')
-api.add_resource(EnderecoId, '/enderecos/<int:id>')
+api.add_resource(EnderecoId, '/enderecos/<id>')
 
 if __name__ == '__main__':
   app.run(debug=True)

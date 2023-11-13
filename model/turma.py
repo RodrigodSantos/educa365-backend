ff968aeed7df5ef7452ffa801241727b4210c8c4
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2023-11-12 21:26:37 -0300

    add patch in turma
    Adição do metodo patch pra adição de professor

--- model/turma.py
+++ model/turma.py
@@ -4,12 +4,14 @@ from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 from model.instituicaoEnsino import instituicaoEnsinoFields
+from model.funcionario import funcionarioFields
 
 turmaFields = {
   "id": fields.String,
   "nome": fields.String,
   "turno": fields.String,
-  "instituicao": fields.Nested(instituicaoEnsinoFields)
+  "instituicao": fields.Nested(instituicaoEnsinoFields),
+  "professor": fields.Nested(funcionarioFields)
 }
 
 class Turma(db.Model):
@@ -17,10 +19,12 @@ class Turma(db.Model):
 
     id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     instituicao_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey("tb_instituicao_ensino.id"), nullable=False)
+    professor_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey("tb_funcionario.pessoa_id"), nullable=True)
     nome = db.Column(db.String, nullable=False)
     turno = db.Column(db.String, nullable=False)
 
     instituicao = db.relationship("InstituicaoEnsino", uselist=False)
+    professor = db.relationship("Funcionario", uselist=False)
 
     def __init__(self, nome, turno, instituicao):
         self.nome = nome
